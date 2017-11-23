f1faadeb3a591d20e77733f471e6362f3c8031b0
Ville-Matti Hakanpää <villematti@villematti.info>
2017-11-23 23:46:10 +0200

    Add restart button

--- www/js/stages/Win.js
+++ www/js/stages/Win.js
@@ -1,12 +1,24 @@
 var winStage = {
+  init: function() {
+
+  },
   preload: function() {
 
   },
   create: function() {
-    scoreText = game.add.text(game.world.width / 2, game.world.width / 2, "You win!");
+    var scoreText = game.add.text(game.world.width / 2, game.world.height / 2, "You win!");
     scoreText.anchor.set(0.5)
+
+
+    var restartText = game.add.text(game.world.width / 2, (game.world.height - 50) / 2, "Tap here to play again");
+    restartText.anchor.set(0.5)
+    restartText.inputEnabled = true;
+    restartText.events.onInputDown.add(this.onDown, this)
   },
   update: function() {
 
+  },
+  onDown: function() {
+    game.state.start('game')
   }
 }
